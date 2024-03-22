e2cfefb103d2a17244f209611287f501993834fc
gumz69 <gumilarsulistian@gmail.com>
2024-03-22 14:34:18 +0700

    sudah ditambahan create

--- src/app/user/tambah-user/tambah-user.component.ts
+++ src/app/user/tambah-user/tambah-user.component.ts
@@ -1,21 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-tambah-user',
   templateUrl: './tambah-user.component.html',
-  styleUrl: './tambah-user.component.css'
+  styleUrl: './tambah-user.component.css',
 })
 export class TambahUserComponent implements OnInit {
   
-  constructor(private httpClient: HttpClient){}
+  constructor(
+    private httpClient: HttpClient,
+    private router: Router){}
 
   ngOnInit(): void {
     initFlowbite();
   }
 
-
+  navigateToUserPage() {
+    this.router.navigate(['/user']);
+}
 
   onTambahUser(users:{
     noRekening: string,
