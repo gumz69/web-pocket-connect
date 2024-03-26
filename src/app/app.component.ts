4c9fb46c29e81beaec998d926dd06dcb559a3bfa
Iko Wirya <ikowirya@gmail.com>
2024-03-26 23:35:36 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { LoginService } from './login/login.service';
 import { HttpClientModule } from '@angular/common/http';
 import { AppService } from './app.service';
-import { SidebarComponent } from "./components/sidebar/sidebar.component";
-import { NavbarComponent } from "./components/navbar/navbar.component";
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    imports: [RouterModule, RouterOutlet, HttpClientModule, SidebarComponent, NavbarComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  imports: [
+    RouterModule,
+    RouterOutlet,
+    HttpClientModule,
+    SidebarComponent,
+    NavbarComponent,
+    CommonModule,
+  ],
 })
 export class AppComponent implements OnInit {
   title = 'pocket-connect';
   isLogin: boolean;
+  isHidden: any = true;
+  isActiveRoute: any;
 
-  constructor(private service: LoginService, private services: AppService) {
+  constructor(private service: LoginService, private services: AppService, private router: Router,) {
     this.isLogin = !!this.service.getAuth();
+    this.isActiveRoute = this.router;
   }
 
   ngOnInit(): void {
+    console.log(this.isActiveRoute.url)
     initFlowbite();
     this.services.getAuth().subscribe({
       next: (data) => {},
