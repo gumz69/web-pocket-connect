8b2da1b147da7f6ca20c895fca148b78f05364c4
gumz69 <gumilarsulistian@gmail.com>
2024-03-28 15:26:40 +0700

    ditambahin validasi lainnya

--- src/app/user/tambah-user/tambah-user.component.ts
+++ src/app/user/tambah-user/tambah-user.component.ts
@@ -10,7 +10,7 @@ import { isEnamChar, isLetterPattern, isMinDuaBelasChar, isMinEnamChar, isNumber
   templateUrl: './tambah-user.component.html',
 })
 export class TambahUserComponent implements OnInit {
-  user: User = {
+   nasabah: User = {
     id: '',
     userId: '',
     noRekening: '',
@@ -23,56 +23,110 @@ export class TambahUserComponent implements OnInit {
   };
   submitted = false;
 
+  isButtonDisabled(): boolean {
+    return !!this.errorMessageNama ||
+           !!this.errorMessageUserIdString ||
+           !!this.errorMessageUserIdMinChar ||
+           !!this.errorMessageNoRekNumber ||
+           !!this.errorMessageNoRekChar ||
+           !!this.errorMessageNoTelpNumber ||
+           !!this.errorMessageNoTelpChar ||
+           !!this.errorMessageNamaIbuKandung ||
+           !!this.errorMessageJenisTabungan ||
+           !!this.errorMessageMpinNumber ||
+           !!this.errorMessageMpinChar;
+  }
+
+  isFormFilled(): boolean {
+    return  !!this.nasabah.nama && 
+            !!this.nasabah.userId &&
+            !!this.nasabah.noRekening &&
+            !!this.nasabah.telepon &&
+            !!this.nasabah.jenisKelamin &&
+            !!this.nasabah.jenisKelamin &&
+            !!this.nasabah.namaIbuKandung &&
+            !!this.nasabah.jenisTabungan &&
+            !!this.nasabah.mpin;
+  }
+
   constructor(private router: Router, private serviceUser: UserService) {}
-  isInput: string = '';
+
 
   errorMessageNama : string | null = null;
   validateNama(event: any) {
-    this.isInput = event.target.value;
-    this.errorMessageNama = isLetterPattern(this.isInput ) ? null : 'Hanya dapat menggunakan huruf';
+    this.nasabah.nama = event.target.value;
+    if (!this.nasabah.nama.trim()) {
+      this.errorMessageNama = '*Nama tidak boleh kosong';
+      return;
+    }
+    this.errorMessageNama = isLetterPattern(this.nasabah.nama ) ? null : '*Hanya dapat menggunakan huruf';
   }
 
   errorMessageUserIdString : string | null = null;
   errorMessageUserIdMinChar: string | null = null;
   validateUserId(event: any) {
-    this.isInput = event.target.value;
-    this.errorMessageUserIdMinChar = isMinEnamChar(this.isInput) ? null : 'Min. 6 Karakter';
+    this.nasabah.userId = event.target.value;
+    if (!this.nasabah.userId.trim()) {
+      this.errorMessageUserIdMinChar = '*User Id tidak boleh kosong';
+      return;
+    }
+    this.errorMessageUserIdMinChar = isMinEnamChar(this.nasabah.userId) ? null : '*Min. 6 Karakter';
   }
 
   errorMessageNoRekNumber : string | null = null;
   errorMessageNoRekChar : string | null = null;
   validateNoRek(event: any) {
-    this.isInput = event.target.value;
-    this.errorMessageNoRekNumber = isNumber(this.isInput) ? null : 'Hanya dapat menggunakan angka';
-    this.errorMessageNoRekChar = isSepuluhChar(this.isInput) ? null : 'Harus 10 Karakter';
+    this.nasabah.noRekening = event.target.value;
+    if (!this.nasabah.noRekening.trim()) {
+      this.errorMessageNoRekNumber = '*Nomor Rekening tidak boleh kosong';
+      return;
+    }
+    this.errorMessageNoRekNumber = isNumber(this.nasabah.noRekening) ? null : '*Hanya dapat menggunakan angka';
+    this.errorMessageNoRekChar = isSepuluhChar(this.nasabah.noRekening) ? null : '*Harus 10 Karakter';
   }
 
   errorMessageNoTelpNumber : string | null = null;
   errorMessageNoTelpChar : string | null = null;
   validateNoTelp(event: any){
-    this.isInput = event.target.value;
-    this.errorMessageNoTelpNumber = isNumber(this.isInput) ? null : 'Hanya dapat menggunakan angka';
-    this.errorMessageNoTelpChar = isMinDuaBelasChar(this.isInput) ? null : 'Min. 12 Karakter';
+    this.nasabah.telepon = event.target.value;
+    if (!this.nasabah.telepon.trim()) {
+      this.errorMessageNoTelpNumber = '*Nomor telepon tidak boleh kosong';
+      return;
+    }
+    this.errorMessageNoTelpNumber = isNumber(this.nasabah.telepon) ? null : '*Hanya dapat menggunakan angka';
+    this.errorMessageNoTelpChar = isMinDuaBelasChar(this.nasabah.telepon) ? null : '*Min. 12 Karakter';
   }
 
   errorMessageNamaIbuKandung : string | null = null;
   validateNamaIbuKandung(event: any){
-    this.isInput = event.target.value;
-    this.errorMessageNamaIbuKandung = isLetterPattern(this.isInput) ? null : 'Hanya dapat menggunakan huruf';
+    this.nasabah.namaIbuKandung = event.target.value;
+    if (!this.nasabah.namaIbuKandung.trim()) {
+      this.errorMessageNamaIbuKandung = '*Nama Ibu Kandung tidak boleh kosong';
+      return;
+    }
+    this.errorMessageNamaIbuKandung = isLetterPattern(this.nasabah.namaIbuKandung) ? null : '*Hanya dapat menggunakan huruf';
   }
 
   errorMessageJenisTabungan : string | null = null;
   validateJenisTabungan(event: any){
-    this.isInput = event.target.value;
-    this.errorMessageJenisTabungan = isLetterPattern(this.isInput) ? null : 'Hanya dapat menggunakan huruf';
+    this.nasabah.jenisTabungan = event.target.value;
+    if (!this.nasabah.jenisTabungan.trim()) {
+      this.errorMessageJenisTabungan = '*Jenis Tabungan tidak boleh kosong';
+      return;
+    }
+    this.errorMessageJenisTabungan = isLetterPattern(this.nasabah.jenisTabungan) ? null : '*Hanya dapat menggunakan huruf';
   }
 
   errorMessageMpinNumber : string | null = null;
   errorMessageMpinChar : string | null = null;
   validateMpin(event: any){
-    this.isInput = event.target.value;
-    this.errorMessageMpinNumber = isNumber(this.isInput) ? null : 'Hanya dapat menggunakan angka';
-    this.errorMessageMpinChar = isEnamChar(this.isInput) ? null : 'Harus 6 Karakter';
+    this.nasabah.mpin = event.target.value;
+    if (!this.nasabah.mpin.trim()) {
+      this.errorMessageNama = '*MPIN tidak boleh kosong';
+      return;
+    }
+    this.errorMessageMpinNumber = isNumber(this.nasabah.mpin) ? null : '*Hanya dapat menggunakan angka';
+    this.errorMessageMpinChar = isEnamChar(this.nasabah.mpin) ? null : '*Harus 6 Karakter';
   }
 
 
@@ -86,11 +140,11 @@ export class TambahUserComponent implements OnInit {
 
   onTambahUser() {
     this.submitted = true;
-    this.serviceUser.createUser(this.user).subscribe({
+    this.serviceUser.createUser(this.nasabah).subscribe({
       next: (data: User) => {
         console.log('User created successfully!');
         this.router.navigate(['/user']);
-        this.user = {
+        this.nasabah = {
           id: '',
           userId: '',
           noRekening: '',
